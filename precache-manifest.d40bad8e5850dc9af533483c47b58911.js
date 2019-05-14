ad237743221be960b47027bf0c942d5d984ca794
Amandeep Singh <amandeep.singh40@ad.infosys.com>
2019-05-14 12:48:52 +0530

    Updates

---
+++ precache-manifest.d40bad8e5850dc9af533483c47b58911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NewsFeeds/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b25a2ab287fb12f0dabb",
+    "url": "/NewsFeeds/static/js/runtime~main.b25a2ab2.js"
+  },
+  {
+    "revision": "3285c55c7d37eec25f4a",
+    "url": "/NewsFeeds/static/js/main.3285c55c.chunk.js"
+  },
+  {
+    "revision": "3cb1ec027afd84bfc18c",
+    "url": "/NewsFeeds/static/js/2.3cb1ec02.chunk.js"
+  },
+  {
+    "revision": "3285c55c7d37eec25f4a",
+    "url": "/NewsFeeds/static/css/main.b30fc1f7.chunk.css"
+  },
+  {
+    "revision": "34320b2d29d13e034bf752acfb92ebae",
+    "url": "/NewsFeeds/index.html"
+  }
+];
